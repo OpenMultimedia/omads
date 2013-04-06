c6d94838623137bcde64c1c410550deaeaa58bac
David Regla <dreglad@gmail.com>
2013-04-05 19:22:34 -0600

    corrección

--- templates/interstitial.js
+++ templates/interstitial.js
@@ -54,7 +54,7 @@ OMAds.insert_banner = function(options) {
     var shown = $$.cookie('OMAds_shown'),
         expires = 1800;
 
-    if (typeof shown == 'undefined' || !shown || ((Number(shown)+expires) > now)) {
+    if (typeof shown == 'undefined' || !shown || ((Number(shown)+expires) < now)) {
 
 	     $$.cookie('OMAds_shown', now);
 
